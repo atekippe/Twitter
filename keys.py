0fed94220d681aec5daddc43fd6aec19c034a451
atekippe <GITSuperuser#1>
2017-08-17 00:09:27 -0500

    Updated API keys...

---
+++ keys.py
@@ -0,0 +1,5 @@
+# Keys to login to Twitter
+consumer_key = 'NrU7iOdBrIHMt2x4A5QxMrbOC'
+consumer_secret = 'Ac5ER7q4ZtECTxKfs1jxxJZvDxd7tzvbWLV8kLg2PefAYR0Dii'
+access_token = '897581286751076353-cBAAESz4lHRNNtBM9ghhtq94yimrf8s'
+access_token_secret = 'dBhgUccI6dsccxYHKtk7snzNi4bAMRHv2Q1FWN5xuDOZb'
