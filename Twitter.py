5d790cf059279d01ef70df6df27d9bbef6e04f41
atekippe <GITSuperuser#1>
2017-08-19 15:07:04 -0500

    Binary done, RoT breaker done

--- Twitter.py
+++ Twitter.py
@@ -3,6 +3,9 @@ import re
 import base64
 import subprocess
 from keys import *
+import enchant
+import time
+import binascii
 
 debug = ''
 
@@ -44,16 +47,48 @@ def rot_decode(story, shift):
         ])
 
 
+def rot_break(data):
+    # get an english Dictionary
+    d = enchant.Dict("en_US")
+    # roll the bits and look for an english word
+    i = 0
+    while i < 26:
+
+        decoded = rot_decode(data, i)
+        is_english = d.check(decoded)
+        print(decoded, is_english)
+        if is_english is True:
+            print(decoded, is_english)
+            return decoded
+            break
+        else:
+            i += 1
+    unknown_write(data)
+    return "Not Cracked"
+
+
+def go_slower():
+    print("Sleeping 3 minutes...")
+    time.sleep(60)
+    print("Sleeping 2 minutes...")
+    time.sleep(60)
+    print("Sleeping 1 minute1...")
+    time.sleep(30)
+    print("Sleeping 30 seconds...")
+    time.sleep(30)
+
+
 def should_we_tweet_live(cracked, tweet_id):
     if "Not Cracked" in cracked:
         print("Loser ", cracked)
     else:
-
         cracked_split = cracked.split()
         to_tweet = "@CipherEveryword " + cracked_split[0]
         try:
             print("Winner  ", cracked)
-            # api.update_status(status=to_tweet, in_reply_to_status_id=tweet_id)
+            # need to slow down the requests - 1 Tweet every 3 minutes on load
+            #go_slower()
+            #api.update_status(status=to_tweet, in_reply_to_status_id=tweet_id)
         except tweepy.TweepError as e:
             print(e)
 
@@ -62,7 +97,10 @@ def crack_stuff(crack_hash, f_format):
 
     file_path = '/tmp/hashes/' + crack_hash
     to_write = crack_hash + ":" + crack_hash
-    clean_up = 'rm ' + file_path + '&& rm /tmp/hashes/test'
+    # get rid of the pot file
+    # clean_up = 'rm ' + file_path + '&& rm /tmp/hashes/test'
+    # Keep the pot file
+    clean_up = 'rm ' + file_path
 
     # Regex to check the John output for success
     filter_cracked = re.compile(crack_hash)
@@ -122,8 +160,10 @@ def crack_stuff(crack_hash, f_format):
 
 def binary_decode(binary_data):
 
-    print(binary_data[2:])
-    #print(chr(int(binary_data[2:], 2)))
+    hex_bin = hex(int((binary_data[2:]), 2))
+    decoded_binary = (binascii.unhexlify(hex_bin[2:]))
+    print("Binary decoded : ", decoded_binary.decode("utf-8"))
+    return decoded_binary.decode("utf-8")
 
 
 # functions to reverse data
@@ -156,6 +196,8 @@ filter_sha224 = re.compile('^[0-9a-fA-F]{56}$')
 filter_sha256 = re.compile('^[0-9a-fA-F]{64}$')
 filter_sha384 = re.compile('^[0-9a-fA-F]{96}$')
 filter_sha512 = re.compile('^[0-9a-fA-F]{128}$')
+# the \ is necessary to escape the '
+filter_caesar = re.compile('^[a-zA-z\']*$')
 
 # Login to Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -163,7 +205,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # Get Tweets for CipherEveryword
-new_tweets = api.user_timeline(screen_name = "CipherEveryword", count=200)
+new_tweets = api.user_timeline(screen_name = "CipherEveryword", count=500)
 
 # Parse the tweets
 for tweet in new_tweets:
@@ -201,7 +243,15 @@ for tweet in new_tweets:
                                 regex = re.search(filter_base64, tweet.text)
                                 if regex is None:
                                     # print("No Match :", tweet.text)
-                                    unknown_write(tweet.text)
+                                    regex = re.search(filter_caesar, tweet.text)
+
+                                    if regex is None:
+                                        # print("No Match :", tweet.text)
+                                        unknown_write(tweet.text)
+                                    else:
+                                        # Try to break the Rot N
+                                        cracked = rot_break(tweet.text)
+                                        should_we_tweet_live(cracked, tweetID)
                                 else:
                                     # Try to decode the b64
                                     cracked = base64_decode(tweet.text)
@@ -244,7 +294,7 @@ for tweet in new_tweets:
                     cracked = crack_stuff(tweet.text, hash_format)
                     should_we_tweet_live(cracked, tweetID)
             else:
-                hash_format = ["ripemd-160", "Raw-SHA1"]
+                hash_format = ["ripemd-160", "Raw-SHA1"]  # , "Raw-SHA1-AxCrypt", "Raw-SHA1-Linkedin", "Raw-SHA1-ng", "has-160"]
 
                 # Debugging info
                 if debug is 'true':
@@ -253,7 +303,7 @@ for tweet in new_tweets:
                 cracked = crack_stuff(tweet.text, hash_format)
                 should_we_tweet_live(cracked, tweetID)
         else:
-            hash_format = ["raw-md5", "raw-md4", "MD2", "ripemd-128", "HAVAL-128-4"]
+            hash_format = ["raw-md5", "raw-md4", "MD2", "ripemd-128", "hmac-md5"]  # , "HAVAL-128-4", "LM", "dynamic=md5($p)", "mdc2", "mscash", "NT", "Raw-MD5u", "Raw-SHA1-AxCrypt", "Snefru-128", "NT-old"]
 
             # Debugging info
             if debug is 'true':
@@ -262,8 +312,9 @@ for tweet in new_tweets:
             cracked = crack_stuff(tweet.text, hash_format)
             should_we_tweet_live(cracked, tweetID)
     else:
-
-        binary_decode(tweet.text)
+        # Submit the decoded Binary
+        cracked = binary_decode(tweet.text)
+        should_we_tweet_live(cracked, tweetID)
 
 
 
